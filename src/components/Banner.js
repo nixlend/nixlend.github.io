c8c75797433b7a32ad781ab3035429e941f82f8e
Nicholas Lee <nicholasle89@hotmail.com>
2019-03-12 22:29:12 -0700

    add image and fix name size on mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,11 @@ class Banner extends Component {
   render() {
     return (
       <div className="banner-container mx-auto mt-5">
+        <img
+          className="rounded-circle mx-auto d-block mb-1"
+          src="./img/nicholas-image.jpg"
+          alt=""
+        />
         <h1 className="banner-name text-center display-3">Nicholas Lee</h1>
       </div>
     );
