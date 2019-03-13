038c0b4db2c8cd417daa5d392af71849b71ca304
Nicholas Lee <nicholasle89@hotmail.com>
2019-03-12 20:54:07 -0700

    adding hover transition effect to icon and name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Banner extends Component {
   render() {
     return (
-      <div className="mx-auto mt-5">
-        <h1 className="text-center display-3">Nicholas Lee</h1>
+      <div className="banner-container mx-auto mt-5">
+        <h1 className="banner-name text-center display-3">Nicholas Lee</h1>
       </div>
     );
   }
